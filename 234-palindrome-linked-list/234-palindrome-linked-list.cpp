702e5e1943d8ed607f5cf80fdea9798f4dd66d9f
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-12 00:10:52 +0530

    Time: 227 ms (93.77%), Space: 113.9 MB (90.60%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,38 +11,58 @@
 class Solution {
     
 public:
-    ListNode* findMid(ListNode* head){
-        ListNode* slow=head,*fast=head;
-        while(fast!=NULL && fast->next!=NULL){
+    ListNode* getMid(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next->next;
+        while(fast && fast->next){
+            fast=fast->next->next;
             slow=slow->next;
-            fast=fast->next;
-            if(fast) fast=fast->next;
+            
         }
         return slow;
     }
-    ListNode* reverseList(ListNode* head){
-        ListNode* prev=NULL,*curr=head,*temp=NULL;
-        while(curr != NULL){
-            temp=curr->next;
+    
+    ListNode* reverse(ListNode* head){
+        
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* forward=NULL;
+        
+        while(curr!=NULL){
+            forward=curr->next;
             curr->next=prev;
             prev=curr;
-            curr=temp;
+            curr=forward;
         }
         return prev;
     }
-    bool compareList(ListNode* head1, ListNode* head2){
-        while(head1!=NULL && head2!=NULL){
-            if(head1->val != head2->val) return false;
-            head1=head1->next;
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        if(head->next==NULL){
+           return true;
+        }
+        
+        ListNode* middle=getMid(head);
+        
+        // ListNode* temp=middle->val;
+        
+        
+        
+        // ListNode* head1=head;
+        ListNode* head2=reverse(middle);
+        
+        while(head2!=NULL && head!=NULL){
+            if(head->val!=head2->val){
+                return false;
+            }
+            head=head->next;
             head2=head2->next;
         }
+        
+//         temp=middle->val;
+        
+//         middle->val=reverse(temp);
         return true;
-    }
-    bool isPalindrome(ListNode* head) {
-        if(head==NULL || head->next==NULL) return true;
-        ListNode* h=head;
-        ListNode* mid = findMid(h);
-        ListNode* h2=reverseList(mid);
-        return compareList(head,h2);
     }
 };
