1e1bd28396a3a631dc0cf447ea292ed1c23b383e
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-16 19:31:47 +0530

    Time: 29 ms (81.87%), Space: 7.2 MB (81.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,54 +1,18 @@
 class Solution {
+    void isPalindrome(const string& s,const int& length,int left,int right, int& start, int& maxlen){
+        while(left>=0 && right<length && s[left]==s[right]) --left,++right;
+        if(maxlen < right-left-1)   maxlen = right-left-1, start = left+1;
+    }
 public:
     string longestPalindrome(string s) {
-       
-        int l_res = 0;
-        int r_res = 0;
-        int res_size = 0;
-        
-        for (int i = 0; i < s.size(); i++){
-            //cout << "odds \n";
-
-            int l = i;
-            int r = i;
-            while ( l >= 0 && r < s.size() && s[l] == s[r]){
-                
-                //cout << r - l + 1 << " " << res_size << '\n';
-                //cout << s[l] << ' ' << s[r] << ' ' << s.substr(l_res, r_res) << "\n";
-                if ((r - l + 1) > res_size){
-                    l_res = l;
-                    r_res = r;
-                    res_size = r - l + 1;
-                }
-                
-                l--;
-                r++;
-            }
-            
+        int n = s.size(),start=0,maxlen=0;
+        if(n<2) return s;
+        for(int i=0;i<n-1;++i){
+            isPalindrome(s,n,i,i,start,maxlen); //aba
+            isPalindrome(s,n,i,i+1,start,maxlen); //noon
         }
-        
-        for (int i = 0; i < s.size(); i++){
-            //cout << "evens \n";
-            int l = i;
-            int r = i+1;
-            while ( l >= 0 && r < s.size() && s[l] == s[r]){
-                
-                //cout << s[l] << ' ' << s[r] << ' ' << s.substr(l_res, r_res) << "\n";
-                if ((r - l + 1) > res_size){
-                    l_res = l;
-                    r_res = r;
-                    res_size = r - l + 1;
-                }
-                
-                l--;
-                r++;
-            }
-            
-        }
-        
-        return s.substr(l_res, res_size);
-        // go to the center
-        
-        
+        return s.substr(start,maxlen);
     }
 };
+
+// EXPAND AROUND CENTER----->APPROACH-1
