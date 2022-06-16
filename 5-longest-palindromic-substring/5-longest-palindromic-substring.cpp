8f7b583861a5e4ca5d89690973c45d22ff3ea03e
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-16 19:24:24 +0530

    Time: 28 ms (83.15%), Space: 7 MB (97.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string manacher_str = "#";
-        for (int i = 0; i<s.size(); i++)
-            manacher_str = manacher_str + s[i] + "#";
-        int len = manacher_str.size();
-        vector<int> RL(len, 0);
-        int pos = 0, maxRight = 0;
-        int maxLen = 0, maxLenPos = 0;//maxLen:
-        for (int i = 0; i<len; i++) {
-            if (i<maxRight)
-                RL[i] = min(RL[2 * pos - i], maxRight - i);
-            else
-                RL[i] = 1;
-            while (i - RL[i] >= 0 && i + RL[i]<len && manacher_str[i + RL[i]] ==manacher_str[i - RL[i]])
-                RL[i]++;
-            if (RL[i]>maxLen) {
-                maxLen = RL[i];
-                maxLenPos = i;
-            }
+       
+        int l_res = 0;
+        int r_res = 0;
+        int res_size = 0;
+        
+        for (int i = 0; i < s.size(); i++){
+            //cout << "odds \n";
 
-            if (i + RL[i]>maxRight) {
-                maxRight = i + RL[i];
-                pos = i;
+            int l = i;
+            int r = i;
+            while ( l >= 0 && r < s.size() && s[l] == s[r]){
+                
+                //cout << r - l + 1 << " " << res_size << '\n';
+                //cout << s[l] << ' ' << s[r] << ' ' << s.substr(l_res, r_res) << "\n";
+                if ((r - l + 1) > res_size){
+                    l_res = l;
+                    r_res = r;
+                    res_size = r - l + 1;
+                }
+                
+                l--;
+                r++;
             }
+            
         }
-        s = "";
-        for (int i = maxLenPos - maxLen + 1; i<maxLenPos + maxLen - 1; i++)
-            if (manacher_str[i] != '#')
-                s += manacher_str[i];
-        return s;
+        
+        for (int i = 0; i < s.size(); i++){
+            //cout << "evens \n";
+            int l = i;
+            int r = i+1;
+            while ( l >= 0 && r < s.size() && s[l] == s[r]){
+                
+                //cout << s[l] << ' ' << s[r] << ' ' << s.substr(l_res, r_res) << "\n";
+                if ((r - l + 1) > res_size){
+                    l_res = l;
+                    r_res = r;
+                    res_size = r - l + 1;
+                }
+                
+                l--;
+                r++;
+            }
+            
+        }
+        
+        return s.substr(l_res, res_size);
+        // go to the center
+        
+        
     }
 };
