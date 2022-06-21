c2f6ec2778b49a219a723e76feac4d8085d6f899
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-21 21:17:48 +0530

    Time: 27 ms (73.33%), Space: 16.5 MB (11.02%) - LeetHub

---
+++ 155-min-stack/155-min-stack.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    struct Node{
+        int val, mn;
+        Node* next;
+        Node(int _val, int _mn, Node*_next = NULL){
+            val = _val;
+            mn = _mn;
+            next = _next;
+        }
+    }*head;
+    void push(int val) {
+        if(!head){
+            head = new Node(val, val);
+        } 
+        else{
+            head = new Node(val, min(val, head->mn), head);
+        }
+    }
+    
+    void pop() {
+        if(!head) return ;
+        Node* temp = head;
+        head = head->next;
+        delete(temp);
+    }
+    
+    int top() {
+        if(!head) return -1;
+        return head->val;
+    }
+    
+    int getMin() {
+        if(!head) return -1;
+        return head->mn;
+    }
+};
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
