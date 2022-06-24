7d7057f6e96829f113b36bf8408595eddc150160
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-24 20:03:02 +0530

    Time: 52 ms (76.22%), Space: 28.8 MB (72.03%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& target) {
-        long long sum=0;
-        int max=0;
-        
-        for(int i=0;i<target.size();i++){
-            sum+=target[i];
-            if(target[max]<target[i]) max=i;
+    bool isPossible(vector<int>& A) {
+        long total = 0;
+        int n = A.size(), a;
+        priority_queue<int> pq(A.begin(), A.end());
+        for (int a : A)
+            total += a;
+        while (true) {
+            a = pq.top(); pq.pop();
+            total -= a;
+            if (a == 1 || total == 1)
+                return true;
+            if (a < total || total == 0 || a % total == 0)
+                return false;
+            a %= total;
+            total += a;
+            pq.push(a);
         }
-        
-        long long diff=sum-target[max];
-         
-        if(target[max]==1 || diff==1) return true;
-        if(diff>target[max] || diff==0 || target[max]%diff==0) return false;
-        
-        target[max]%=diff;
-        
-        return isPossible(target);
-        
     }
 };
