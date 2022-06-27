9b85d8e305b4c20de2dfae381dd98b56972772bc
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-27 21:06:05 +0530

    Time: 3 ms (74.21%), Space: 8.7 MB (59.29%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-          int eats0s = 0;
-        
-        for (int n : students) {
-            if (n == 0) {
-                eats0s++;
+          int size = students.size();
+        queue<int> student_choice;
+        for(int i = 0 ; i < size ; ++i){
+            student_choice.push(students[i]);
+        }
+        int rotations = 0 , i = 0;
+        while(student_choice.size() && rotations < student_choice.size()){
+            if(student_choice.front() == sandwiches[i]){
+                student_choice.pop();
+                i++;
+                rotations = 0;
+            } else {
+                int choice = student_choice.front();
+                student_choice.pop();
+                student_choice.push(choice);
+                rotations++;
             }
         }
-        int eats1s = students.size() - eats0s;
-        int numSandwiches = sandwiches.size();
-        
-        for (int i=0; i<numSandwiches; ++i) {
-            if (sandwiches[i] == 0) {
-                if (eats0s == 0) {
-                    // if we get a 0 sandwich and no one is left who
-                    // eats 0s, then everyone left goes hungry because the top
-                    // sandwich blocks them.
-                    return (eats0s + eats1s);
-                }
-                else {
-                    eats0s--;
-                }
-            }
-            else {
-                if (eats1s == 0) {
-                    return (eats0s + eats1s);
-                }
-                else {
-                    eats1s--;
-                }
-            }
-        }
-        
-        return 0;
+        return student_choice.size();
     }
 };
