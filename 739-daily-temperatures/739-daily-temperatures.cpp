6ab7f3604f8497d7e913b4d633521866dfb546c0
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-06-30 22:23:47 +0530

    Time: 232 ms (56.66%), Space: 85 MB (86.52%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -3,24 +3,24 @@ public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         
         int n=temp.size();
-        
+        int hottest=INT_MIN;
         vector<int> ans(n,0);
         
         for(int i=n-1;i>=0;i--){
             
-            int j=i+1;
-            while(j<n && temp[j]<=temp[i]){
-                if(ans[j]>0){
+           if(hottest<=temp[i]){
+               hottest=temp[i];
+           }
+            
+            else{
+                int j=i+1;
+                while(temp[j]<=temp[i]){
                     j+=ans[j];
                 }
-                else{
-                    j=n;
-                }
-            }
-         
-            if(j<n){
+                
                 ans[i]=j-i;
             }
+            
         }
         
         return ans;
