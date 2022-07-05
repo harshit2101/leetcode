8cd4eaa0dc8532db324e1f006357b235e592237c
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-05 11:48:27 +0530

    Time: 2 ms (61.90%), Space: 8.3 MB (95.78%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -14,16 +14,23 @@ public:
     vector<int> postorderTraversal(TreeNode* root) {
         
         vector<int> nodes;
-        postorder(root,nodes);
+        stack<TreeNode*> todo;
+        TreeNode* last = NULL;
+        while (root || !todo.empty()) {
+            if (root) {
+                todo.push(root);
+                root = root -> left;
+            } else {
+                TreeNode* node = todo.top();
+                if (node -> right && last != node -> right) {
+                    root = node -> right;
+                } else {
+                    nodes.push_back(node -> val);
+                    last = node;
+                    todo.pop();
+                }
+            }
+        }
         return nodes;
-    }
-    
-private:
-    void postorder(TreeNode* root, vector<int>& nodes){
-        if(root==NULL) return;
-        
-        postorder(root->left,nodes);
-        postorder(root->right,nodes);
-        nodes.push_back(root->val);
     }
 };
