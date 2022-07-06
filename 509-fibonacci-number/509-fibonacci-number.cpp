a2d0a179fb287addc82627bc4e6533ef24dd1e39
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-06 10:53:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n<2){
+            return n;
+        }
+        int x=0;
+        int y=1;
+        
+        for(int i=2;i<=n;i++){
+            y=x+y;
+            x=y-x;
+        }
+        return y;
+    }
+};
