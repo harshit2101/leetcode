2c771ef35c5b4ff2c3745db94eefee50933ab421
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-07 11:11:01 +0530

    Time: 7 ms (40.05%), Space: 12 MB (76.12%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,37 +13,33 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         
-        if (root == NULL) {
-        return vector<vector<int> > ();
-    }
-    vector<vector<int> > result;
-
-    queue<TreeNode*> nodesQueue;
-    nodesQueue.push(root);
-    bool leftToRight = true;
-
-    while ( !nodesQueue.empty()) {
-        int size = nodesQueue.size();
-        vector<int> row(size);
-        for (int i = 0; i < size; i++) {
-            TreeNode* node = nodesQueue.front();
-            nodesQueue.pop();
-
-            // find position to fill node's value
-            int index = (leftToRight) ? i : (size - 1 - i);
-
-            row[index] = node->val;
-            if (node->left) {
-                nodesQueue.push(node->left);
+        vector<vector<int>> ans;
+        if(root==NULL)return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        bool leftToright=true;
+        
+        while(!q.empty()){
+            
+            int n=q.size();
+            vector<int> temp(n);
+            
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                
+                int index=(leftToright)? i: (n-i-1);
+                temp[index]=node->val;
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-            if (node->right) {
-                nodesQueue.push(node->right);
-            }
+            leftToright=!leftToright;
+            ans.push_back(temp);
         }
-        // after this level
-        leftToRight = !leftToRight;
-        result.push_back(row);
+        
+        return ans;
     }
-    return result;
-}
 };
