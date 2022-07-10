828a3078ebc80bc7faefa003696688b9873a1cf7
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-10 14:59:23 +0530

    Time: 114 ms (43.62%), Space: 28.8 MB (42.34%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
     
-    void heapify(vector<int>& nums, int n, int i){
-        int big=i, left=2*i+1, right=2*i+2;
-        if(left<n && nums[left]>nums[big]){
-            big=left;
+   void merge(vector<int>& nums, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int A[n1], B[n2];
+
+        for(int i = 0; i < n1; i++)
+            A[i] = nums[l + i];
+
+        for(int i = 0; i < n2; i++)
+            B[i] = nums[m + 1 + i];
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while(i < n1 && j < n2)
+        {
+            if(A[i] <= B[j])
+                nums[k++] = A[i++];
+            else
+                nums[k++] = B[j++];
         }
-        if(right<n && nums[right]>nums[big]){
-            big=right;
-        }
-        if(big!=i){
-            swap(nums[i],nums[big]);
-            heapify(nums,n,big);
-        }
+
+        while(i < n1)
+            nums[k++] = A[i++];
+        while(j < n2)
+            nums[k++] = B[j++];
     }
-    void buildHeap(vector<int>& nums, int n){
-        for(int i=(n-2)/2;i>=0;i--){
-            heapify(nums,n,i);
-        }
+
+    void mergeSort(vector<int>& nums, int l, int r)
+    {
+        if(l >= r)  //remember to put the equal to sign
+            return;
+
+        int m = l + (r - l)/2;
+        mergeSort(nums, l, m);
+        mergeSort(nums, m + 1, r);
+        merge(nums, l, m, r);
     }
-    void heapsort(vector<int>& nums, int n){
-        buildHeap(nums,n);
-        for(int i=n-1;i>=0;i--){
-            swap(nums[i],nums[0]);
-            heapify(nums,i,0);
-        }
-    }
-    vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        heapsort(nums,n);
+
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
