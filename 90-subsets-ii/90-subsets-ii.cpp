06744c43dd2e9271d63857b06a2a79c21bd0a958
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-13 10:28:39 +0530

    Time: 0 ms (100.00%), Space: 8 MB (42.35%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    void solve(vector <int> &nums,int idx,vector <int> &res, vector <vector<int>> &ans)
-    {
-        ans.push_back(res);
-        for(int i=idx;i<nums.size();i++){
-            if(i!=idx && nums[i]==nums[i-1])
-                continue;
-            res.push_back(nums[i]);
-            solve(nums,i+1,res,ans);
-            res.pop_back();
-        } 
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector <vector <int> >ans;
-        vector <int> res;
-        sort(nums.begin(),nums.end());
-        solve(nums,0,res,ans);
-        return ans;
+        set<vector<int>> ans;
+        vector<int> curr;
+        vector<vector<int>> ret;
+        
+        sort(nums.begin(), nums.end());
+        backtrack(ans, curr, nums, 0);
+        ret.assign(ans.begin(), ans.end());
+        return ret;
+    }
+    void backtrack(set<vector<int>>& ans, vector<int>& curr, vector<int>& nums, int pos) {
+        if(pos == nums.size()) {
+            ans.insert(curr);
+            return;
+        }
+        curr.push_back(nums[pos]);
+        backtrack(ans, curr, nums, pos + 1);
+        curr.pop_back();
+        backtrack(ans, curr, nums, pos + 1);
     }
 };
