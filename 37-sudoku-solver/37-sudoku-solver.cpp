2ae526921d8be2e18361b9357e95557836c15190
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-14 11:09:31 +0530

    Time: 49 ms (41.72%), Space: 6.3 MB (79.95%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     
-   void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+    bool isSafe(char ch,vector<vector<char>>& board,int row,int col){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==ch) return false;
+            if(board[i][col]==ch) return false;
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == ch) return false;
+        }
+        return true;
     }
     
     bool solve(vector<vector<char>>& board){
-        for(int i=0; i<board.size(); i++){
-            for(int j=0; j<board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    for(char c='1'; c<='9'; c++){
-                        if(valid(c, board, i, j)){
-                            board[i][j] = c;
-                            if(solve(board) == true) return true;
+        int n=board.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isSafe(ch,board,i,j)){
+                            board[i][j]=ch;
+                            if(solve(board)) return true;
                             else board[i][j]='.';
                         }
                     }
@@ -23,12 +29,8 @@ public:
         return true;
     }
     
-    bool valid(char c, vector<vector<char>>& board, int row, int col){
-        for(int i=0; i<9; i++){
-            if(board[i][col] == c) return false;
-            if(board[row][i] == c) return false;
-            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c) return false;
-        }
-        return true;
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
 };
+
