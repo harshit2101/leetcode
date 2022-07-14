976988532add2ef4b44b4d7229384a86041d8286
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-14 10:32:39 +0530

    Time: 44 ms (40.32%), Space: 25.9 MB (79.05%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,32 +12,22 @@
 class Solution {
 public:
     
-    void findIndex(vector<int>in,map<int,int>& m){
-        for(int i=0;i<in.size();i++){
-            m[in[i]]=i;
-        }
-    }
-    
-    TreeNode* solve(vector<int> pre, vector<int> in,int inStart,int inEnd,int& p_index,int n,map<int,int>& m){
-        if(p_index>=n || inStart>inEnd){
-            return NULL;
-        }
-        int element=pre[p_index++];
-        TreeNode* root=new TreeNode(element);
-        int pos=m[element];
+   TreeNode* solve(int start_pre, int start_in, int end_in, vector<int>& preorder, vector<int>& inorder) 
+    {
         
-        root->left=solve(pre,in,inStart,pos-1,p_index,n,m);
-        root->right=solve(pre,in,pos+1,inEnd,p_index,n,m);
+        if(start_in > end_in || start_pre > preorder.size()-1) return NULL;
         
+        TreeNode* root = new TreeNode(preorder[start_pre]);
+        
+        int index = 0;
+        for(int i = start_in; i <= end_in; i++)
+            if(root->val == inorder[i]) index = i;
+            
+        root->left = solve(start_pre+1, start_in, index-1, preorder, inorder);
+        root->right = solve(start_pre+index-start_in+1, index+1, end_in, preorder, inorder);
         return root;
     }
-    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int p_index=0;
-        int n=inorder.size();
-        map<int,int> m;
-        findIndex(inorder,m);
-        TreeNode* ans=solve(preorder,inorder,0,n-1,p_index,n,m);
-        return ans;
+       return solve(0, 0, inorder.size()-1, preorder, inorder);
     }
 };
