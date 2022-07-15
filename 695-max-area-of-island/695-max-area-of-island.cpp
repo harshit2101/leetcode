15416bd244c26c32c5d70fe0e4500d7f65a24a4a
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-15 10:23:14 +0530

    Time: 44 ms (19.86%), Space: 26.9 MB (13.01%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    vector<pair<int,int>>dir={{1,0},{-1,0},{0,-1},{0,1}};
+    int dir[5]= {0, 1, 0, -1, 0};
+    bool valid(int x,int y,vector<vector<int>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==1)return true;
+        return false;
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int N=grid.size();
-        int M=grid[0].size();
-        int m_area=0;
-        
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(grid[i][j]==0)continue;
-                queue<pair<int,int>>q;
-                q.push({i,j});
-                int area=0;
-                while(!q.empty()){
-                    int n=q.size();
-                    while(n--){
-                        pair<int,int>p=q.front();
+       int n = grid.size();
+       int m = grid[0].size();
+        int maxm = INT_MIN;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                int c=0;
+                if(grid[i][j]){
+                    queue<pair<int,int>>q;
+                    grid[i][j]=0;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto src = q.front();
+                        
                         q.pop();
-                        grid[p.first][p.second]=0;
-                        for(auto k:dir){
-                            int x=p.first+k.first;
-                            int y=p.second+k.second;
-                            
-                            if(x>=0 and y>=0 and x<N and y<M and grid[x][y]==1){
-                                grid[x][y]=0;
-                                q.push({x,y});
-                                area++;
+                        c++;
+                        for(int i = 0;i<4;i++){
+                        int dx = dir[i]+src.first;
+                        int dy = dir[i+1]+src.second;
+                            if(valid(dx,dy,grid)){
+                                
+                                q.push({dx,dy});
+                                grid[dx][dy]=0;
                             }
                         }
                     }
+                  
                 }
-                m_area=max(area+1,m_area);
+                  maxm = max(maxm,c);
             }
         }
-        return m_area;
+        return maxm;
     }
 };
