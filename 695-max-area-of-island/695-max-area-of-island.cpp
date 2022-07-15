6cf6433d86da66b980d1e115882306b95b88ecd0
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-15 10:12:57 +0530

    Time: 30 ms (54.31%), Space: 26.8 MB (13.01%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    
-    int solve(vector<vector<int>>& grid,int r,int c){
-        if(r>=0 && r<grid.size() && c>=0 && c<grid[0].size() && grid[r][c]==1){
-            grid[r][c]=0;
-            return (1+ solve(grid,r+1,c) + solve(grid,r-1,c) + solve(grid,r,c+1) + solve(grid,r,c-1));
-        }
-        return 0;
-    }
-    
+    vector<pair<int,int>>dir={{1,0},{-1,0},{0,-1},{0,1}};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans=max(ans,solve(grid,i,j));
+        int N=grid.size();
+        int M=grid[0].size();
+        int m_area=0;
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(grid[i][j]==0)continue;
+                queue<pair<int,int>>q;
+                q.push({i,j});
+                int area=0;
+                while(!q.empty()){
+                    int n=q.size();
+                    while(n--){
+                        pair<int,int>p=q.front();
+                        q.pop();
+                        grid[p.first][p.second]=0;
+                        for(auto k:dir){
+                            int x=p.first+k.first;
+                            int y=p.second+k.second;
+                            
+                            if(x>=0 and y>=0 and x<N and y<M and grid[x][y]==1){
+                                grid[x][y]=0;
+                                q.push({x,y});
+                                area++;
+                            }
+                        }
+                    }
+                }
+                m_area=max(area+1,m_area);
             }
         }
-        return ans;
+        return m_area;
     }
 };
