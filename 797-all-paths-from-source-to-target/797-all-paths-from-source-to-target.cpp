01aeb97228dc4c98d8310a284982d9e9bc2fe8ec
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-16 13:17:36 +0530

    Time: 22 ms (59.37%), Space: 10.4 MB (98.79%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    void solve(vector<vector<int>>& graph, vector<vector<int>>& ans,vector<int>& temp,int node){
+        temp.push_back(node);
+        
+        if(node==graph.size()-1) ans.push_back(temp);
+        
+        for(auto it: graph[node]){
+            solve(graph,ans,temp,it);
+        }
+        temp.pop_back();
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(graph,ans,temp,0);
+        return ans;
+    }
+};
