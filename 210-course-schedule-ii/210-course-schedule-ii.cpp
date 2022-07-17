629967ef188ad715922996dade1c31f5c52a2efb
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-17 17:41:25 +0530

    Time: 29 ms (67.74%), Space: 14.9 MB (23.26%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,54 +1,41 @@
 class Solution {
 public:
     
-    bool detectCycle_util(vector<vector<int>>& adj, int cur,vector<int>& visited)
-    {
-        if(visited[cur]==1) return true;
-        if(visited[cur]==2) return false;
-        
-        visited[cur]=1;
-        for(auto it:adj[cur])
-        {
-            if(detectCycle_util(adj,it,visited)) return true;
+    void dfs(int node,vector<int>adj[],stack<int>&st,vector<int>&vis,vector<int>&cycle,bool &ispos){
+        vis[node]=1;
+        cycle[node]=1;
+        for(auto i:adj[node]){
+            if(!vis[i]){
+                dfs(i,adj,st,vis,cycle,ispos);
+            }else if(vis[i] && cycle[i]){
+                ispos=false;
+            }
         }
-        visited[cur]=2;
-        return false;
-    }
-    bool iscycle(vector<vector<int>>& adj,int numco)
-    {
-        vector<int> visited(numco,0);
-        for(int i=0;i<numco;i++)
-        {
-            if(visited[i]!=1)
-                if(detectCycle_util(adj,i,visited))
-                    return true;
-        }
-        return false;
-    }
-    void toposort(int node,vector<vector<int>>&adj, stack<int>&s,  vector<int>&visited){
-        visited[node]=1;
-        for(auto it:adj[node]){
-            if(visited[it]==0)
-                toposort(it,adj,s,visited);
-        }
-        s.push(node);
+        cycle[node]=0;
+        st.push(node);
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>ans;
-        vector<vector<int>>adj(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        vector<int>adj[numCourses];
+        for(auto i:prerequisites){
+            int a=i[0];
+            int b=i[1];
+            adj[b].push_back(a);
         }
-        vector<int>visited(numCourses,0);
-        if(iscycle(adj,numCourses)) return {};
-        visited.clear();
-        stack<int>s;
-        for(int i=0;i<numCourses;i++)
-            if(visited[i]==0)
-            toposort(i,adj,s,visited);
-        while(!s.empty()){
-            ans.push_back(s.top());
-            s.pop();
+        stack<int>st;
+        vector<int>vis(numCourses,0);
+        vector<int>cycle(numCourses,0);
+        bool ispos=true;
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                dfs(i,adj,st,vis,cycle,ispos);
+            }
+        }
+        vector<int>ans;
+        if(ispos){
+            while(!st.empty()){
+                ans.push_back(st.top());
+                st.pop();
+            }
         }
         return ans;
     }
