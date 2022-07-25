5db8247d9f4b8893b208c906d7f4164084a743a5
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-25 09:46:50 +0530

    Time: 18 ms (69.00%), Space: 9.2 MB (55.66%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -3,30 +3,28 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         
         int n = nums.size();
+        int sum = 0;
         
-        float total_sum = 0;
-        for(auto& n: nums) total_sum+=n;
+        for(int i=0;i<n;i++) sum+=nums[i];
+        int m = sum + target;
+        
+        if(m%2!=0 || m<0) return 0;
         
         
-        float o_sum = (total_sum+target)/2;
-        if(o_sum < 0) return 0;
+        m = m/2;
+        int dp[21][2001];
+        memset(dp, 0, sizeof(dp));
         
-        int i_sum = o_sum;
-        float f_sum = i_sum;
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
+        for(int j=1;j<=m;j++) dp[0][j] = 0;
         
-        if(f_sum != o_sum) return 0;
-        int sum = i_sum;
         
-        int dp[n+1][sum+1];
-        
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=sum; j++){
-                if(i==0) dp[i][j] = (j==0);
-                else if(nums[i-1] > j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(nums[i-1]<=j) dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
             }
         }
-        
-        return dp[n][sum];
+        return dp[n][m];
     }
 };
