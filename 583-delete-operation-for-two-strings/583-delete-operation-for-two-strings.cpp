0f958a8ed35ae01de1c908b491607f8a6291a49c
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-27 15:31:27 +0530

    Time: 103 ms (15.34%), Space: 8.9 MB (59.63%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int m=word1.length(),n=word2.length();
-		//m= lenght of word1 string & n is also
+    
+    
+    int lcs(string text1, string text2) {
         
-		int dp[m+1][n+1];
-		//created m and n size table
-		
-        for(int i=0;i<=m;i++)
-            dp[i][0]=0;
-		//first column is seted to zero	
-		
-          for(int i=0;i<=n;i++)
-            dp[0][i]=0;
-        //first row is seted to zero
-		//because initially no common sequence
-		
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-				//here i compared last elements of both string
-                if(word1[i-1]==word2[j-1])
+        int m=text1.size();
+        int n=text2.size();
+        int dp[1001][1001];
+        
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+        
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(text1[i-1]==text2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
-					//if its same then we added +1 to last result
-                else
-                {
-	
-                    dp[i][j]=max(dp[i-1][j],max(dp[i][j-1],dp[i-1][j-1]));
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-		//here i found dp[m][n] which is common sequence and then i substracted it from both string length and we get result
-        int sum = (m- dp[m][n])+(n-dp[m][n]);
-                return sum;
+        
+        return dp[m][n];
+    }
+    
+    int minDistance(string word1, string word2) {
+        
+        int m=word1.size();
+        int n=word2.size();
+        
+        return m+n - 2*(lcs(word1,word2));
+        
     }
 };
