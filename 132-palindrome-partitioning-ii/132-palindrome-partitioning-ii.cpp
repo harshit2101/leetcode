a1a3a1e2767d76959a95422d7ac350971929d116
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-07-28 11:36:24 +0530

    Time: 648 ms (25.31%), Space: 24 MB (42.38%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int dp [2001][2001];
+private:
+    bool ispalindrome(string &s,int i,int j){
+       if(i>=j){
+            return true;
+        }
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+private:
+    int solve(string &s,int i,int j){
+        if(i>=j){
+            return 0;
+        }
+        if(ispalindrome(s,i,j)){
+            return dp[i][j] =0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int mn = INT_MAX;
+        for(int k =i;k<j;k++){
+            if(ispalindrome(s,i,k)){
+            int temp =solve(s,i,k)+solve(s,k+1,j)+1;
+            mn = min(temp,mn);
+            }
+        }
+        return dp[i][j] =mn;
+
+    }
+public:
+    int minCut(string s) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0,s.size()-1);
+
+    }
+};
