eb632505e8abad2e7cb62585149875430305135c
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-08-04 10:37:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int calc(int n,vector<int> &dp)
     {
-        if(n<0)
-            return 0;
+        
         if(n==0 || n==1)
             return 1;
         if(dp[n]!=-1)
