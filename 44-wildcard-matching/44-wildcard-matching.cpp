a74fa2c07a069d269d23483df4377e222d02bc58
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-08-09 14:42:22 +0530

    Time: 84 ms (58.95%), Space: 27.1 MB (39.39%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -2,35 +2,64 @@ class Solution {
 public:
     
     
-    bool helper(int i,int j,string &p,string &s,vector<vector<int>> &dp)
-    {
-        if(i<0 && j<0) return true;
+//     bool helper(int i,int j,string &p,string &s,vector<vector<int>> &dp)
+//     {
+//         if(i<0 && j<0) return true;
         
-        if(i<0 && j>=0) return false;
+//         if(i<0 && j>=0) return false;
         
-        if(j<0 && i>=0)
+//         if(j<0 && i>=0)
+//         {
+//             for(int k=0;k<=i;++k)
+//             {
+//                 if(p[k]!='*')
+//                     return false;
+//             }
+//             return true;
+//         }
+        
+//         if(dp[i][j]!=-1) return dp[i][j];
+        
+//         if(p[i]==s[j] || p[i]=='?') return dp[i][j]=helper(i-1,j-1,p,s,dp);
+        
+//         if(p[i]=='*') return dp[i][j]=helper(i-1,j,p,s,dp) || helper(i,j-1,p,s,dp);
+        
+//         return dp[i][j]=false;
+//     }
+    bool isMatch(string s, string p) {
+        int m=s.size(), n=p.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,false));
+        
+        dp[0][0]=true;
+        
+        for(int j=1;j<=m;j++) dp[0][j]=false;
+        
+        for(int i=1;i<=n;++i)
         {
-            for(int k=0;k<=i;++k)
+            bool flag=true;
+            for(int k=1;k<=i;++k)
             {
-                if(p[k]!='*')
-                    return false;
+                if(p[k-1]!='*')
+                {
+                    flag=false;
+                    break;
+                }
             }
-            return true;
+            dp[i][0]=flag;
         }
         
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(p[i]==s[j] || p[i]=='?') return dp[i][j]=helper(i-1,j-1,p,s,dp);
-        
-        if(p[i]=='*') return dp[i][j]=helper(i-1,j,p,s,dp) || helper(i,j-1,p,s,dp);
-        
-        return dp[i][j]=false;
-    }
-    bool isMatch(string s, string p) {
-        int m=s.size(),n=p.size();
-        
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        
-        return helper(n-1,m-1,p,s,dp);
+        for(int i=1;i<=n;++i)
+        {
+            for(int j=1;j<=m;++j)
+            {
+                if(p[i-1]==s[j-1] || p[i-1]=='?')
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*')
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                else
+                    dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 };
