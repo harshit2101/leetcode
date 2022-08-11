fd04e42f6285d91268f63ed29546f7dc136d7a4c
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-08-11 10:42:46 +0530

    Time: 224 ms (27.68%), Space: 13.1 MB (90.49%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,41 +1,44 @@
+
 class Solution {
-    bool compare(string &s1, string &s2) {
-        
-        int i = 0, j = 0;
-        int n1 = s1.size(), n2 = s2.size();
-        if(n2 - 1 != n1) return false;
-        while(j < n2) {
-            if(s1[i] == s2[j])
-                i++, j++;
-            else 
-                j++;
+public:
+    
+    bool check(string& s1, string& s2){
+        if(s1.size()!= s2.size()+1) return false;
+        int first=0,second=0;
+        while(first<s1.size()){
+            if(s1[first]==s2[second]){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
         }
         
-        if(i == n1 and j == n2)
-            return true;
-        
+        if(first==s1.size() && second==s2.size()) return true;
         return false;
     }
-public:
-    static bool comp(string &s1, string &s2) {
-        return s1.size() < s2.size();
+    
+    
+    static bool comp(string& a, string& b){
+        return a.size()<b.size();
     }
+    
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), comp);
-      
-        int maxi = 1;
         int n = words.size();
-        vector<int> dp(n, 1);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-
-                if(compare(words[j], words[i]) and dp[i] < dp[j] + 1)
-                    dp[i] = dp[j] + 1;
-            }
-            maxi = max(maxi, dp[i]);
-
-
+        vector<int> dp(n,1);
+        int maxi=1;
+        
+        sort(words.begin(),words.end(),comp);
+        
+        for(int ind = 1;ind<n;ind++){
+            for(int prev =0;prev<ind;prev++){
+                if(check(words[ind], words[prev])){
+                    dp[ind]=max(dp[ind],1+dp[prev]);
+                }
+             }
+            maxi = max(maxi,dp[ind]);
         }
         return maxi;
-    }
+	}
 };
