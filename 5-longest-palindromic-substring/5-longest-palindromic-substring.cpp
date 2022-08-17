fa855d35b774747c2c00b32f6e6ad06ed10f2449
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-08-17 22:31:17 +0530

    Time: 1162 ms (9.85%), Space: 231.1 MB (22.27%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,18 +1,44 @@
 class Solution {
-    void isPalindrome(const string& s,const int& length,int left,int right, int& start, int& maxlen){
-        while(left>=0 && right<length && s[left]==s[right]) --left,++right;
-        if(maxlen < right-left-1)   maxlen = right-left-1, start = left+1;
-    }
 public:
     string longestPalindrome(string s) {
-        int n = s.size(),start=0,maxlen=0;
-        if(n<2) return s;
-        for(int i=0;i<n-1;++i){
-            isPalindrome(s,n,i,i,start,maxlen); //aba
-            isPalindrome(s,n,i,i+1,start,maxlen); //noon
+        int n = s.size();
+		
+       
+        string t = s;
+        reverse(t.begin(), t.end());
+        
+		
+        int dp[n+1][n+1];
+        int mx = INT_MIN;
+        string ans = "";
+        
+        for(int i=0 ;i<n+1; ++i)
+            for(int j=0; j<n+1; ++j)
+                if(i==0 || j==0)
+                    dp[i][j] = 0;
+        
+        for(int i=1 ;i<n+1; ++i){
+            for(int j=1; j<n+1; ++j){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    
+                    
+                    if(dp[i][j] > mx){
+                        string temp=s.substr(i-dp[i][j],dp[i][j]);
+                        string revtemp=temp;
+                        reverse(revtemp.begin(),revtemp.end());
+                        if(revtemp==temp){
+                            mx=dp[i][j];
+                            ans=temp;
+                        }
+                    }
+                    
+                }
+                else{
+                    dp[i][j] = 0; 
+                }
+            }
         }
-        return s.substr(start,maxlen);
+        return ans;
     }
 };
-
-// EXPAND AROUND CENTER----->APPROACH-1
