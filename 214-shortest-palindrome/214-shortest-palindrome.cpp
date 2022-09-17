ff07c6e7bb0d0ee808108dfa9fbfe283cb8dfdbc
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-09-17 11:44:33 +0530

    Time: 11 ms (25.27%), Space: 8.1 MB (35.71%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-		string t=s;
-		reverse(t.begin(),t.end());
-		string str=s+"#"+t;
-		int n=str.length();
-		int i=1,j=0;
-		vector<int> kmp(n);
-		while(i<n) {
-			if(j<n&&str[i]==str[j]) {
-				kmp[i++]=++j;
-			}
-			else {
-				if(j!=0) j=kmp[j-1];
-				else kmp[i++]=j;
-			}
-		}
-		string l=s.substr(kmp[n-1]);
-		reverse(l.begin(),l.end());
-		return l+s;
-	}
+        string t = s+"$";
+        reverse(s.begin(),s.end());
+        t = t+s;
+        int n = t.size();
+        vector<int>lps(n,0);
+        int i=1,j=0;
+        while(i<n && j<n){
+            if(t[i]==t[j]){
+                lps[i] = j+1;
+                i++;j++;
+                
+            }
+            else{
+                if(j>0){
+                    j = lps[j-1];
+                }
+                else{
+                    i+=1;
+                }
+            }
+        }
+        
+        string str = s.substr(0,s.size()-lps[n-1]);
+        reverse(s.begin(),s.end());
+        
+        str = str+s;
+        
+        return str;
+    }
 };
