135869b82c291439a97411db2f8569092de768ff
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-12 14:13:06 +0530

    Time: 91 ms (85.52%), Space: 12.5 MB (61.82%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
+        vector<vector<int>> matrix(n, vector<int>(n,INT_MAX/2));
+        
+        for(auto it:edges){
+            matrix[it[0]][it[1]]=it[2];
+            matrix[it[1]][it[0]]=it[2];
+        }
+        
+        // for(int i=0;i<n;i++) matrix[i][i]=0;
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=min(matrix[i][j], matrix[i][k]+matrix[k][j]);
+                }
+            }
+        }
+        
+        int mincount=INT_MAX, ans=-1;
+        
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(i!=j && matrix[i][j]<=distanceThreshold) count++;
+            }
+            if(count<=mincount){
+                mincount=count;
+                ans=max(ans,i);
+                // ans=i;
+            }
+        }
+        
+        return ans;
+    }
+};
+
