37a8ad382ffd3a8cb8be4d1e0b787305c9f05125
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-17 10:47:15 +0530

    Time: 63 ms (88.11%), Space: 31.5 MB (69.26%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -6,10 +6,13 @@ public:
         int runningSum = 0;
         int ans = 0;
         unordered_map<int, int> freq;
-        freq[0] = 1;
+        // freq[0] = 1;
         for(int i = 0; i < n; i++) {
             runningSum += nums[i];
             int rem = runningSum % k;
+            
+            if(rem==0) ans++;
+            
             if (rem < 0) {
                 rem += k;
             }
