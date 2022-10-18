78f3b3d2180d62af40e3cc8b39610da610ec6702
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-18 19:51:59 +0530

    Time: 16 ms (11.58%), Space: 9.6 MB (71.74%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,6 +4,9 @@ public:
         
         int s=0,e=nums.size()-1;
         
+        if(target<nums[0]) return 0;
+        if(target>nums[e]) return e+1;
+        
         while(s<=e){
             int mid=s + (e-s)/2;
             
