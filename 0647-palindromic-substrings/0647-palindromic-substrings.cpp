487e8bd805221b7cec40d52443effac0debaec32
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-21 19:00:52 +0530

    Time: 12 ms (62.86%), Space: 13.2 MB (31.25%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int solve(string s, int i,int j,int &count){
+        while(i>=0 && i<s.size() && j>=0 && j<s.size() && s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
+        
+        return count;
+    }
+    
+    int countSubstrings(string s) {
+        
+        int count=1;
+        
+        for(int i=0;i<s.size()-1;i++){
+            int odd=solve(s,i,i,count);
+            int even=solve(s,i,i+1,count);
+        }
+        
+        return count;
+    }
+};
