389a5a8e8efe0a8acc4e6ce0a58ec66cda95012f
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-26 12:02:28 +0530

    Time: 581 ms (18.68%), Space: 112.4 MB (94.95%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,18 +3,18 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         
         int prefsum=0;
-        unordered_map<int,int> m;
+        unordered_map<int,int> m={{0,-1}};
         
         for(int  i=0;i<nums.size();i++){
             prefsum+=nums[i];
-            prefsum%=k;
+            // prefsum%=k;
             
-            if(prefsum==0 && i) return true;
+            // if(prefsum==0 && i) return true;
             
-            if(m.find(prefsum)!=m.end()){
-                if(i-m[prefsum]>1) return true;
+            if(m.find(prefsum%k)!=m.end()){
+                if(i-m[prefsum%k]>1) return true;
             }
-            else m[prefsum]=i;
+            else m[prefsum%k]=i;
         }
         
         return false;
