97b9c84de98ce0d808c3672ef0076feb141d8021
harshit2101 <96678131+harshit2101@users.noreply.github.com>
2022-10-30 11:01:15 +0530

    Time: 12 ms (98.15%), Space: 10.1 MB (85.42%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,45 +1,41 @@
 class Solution {
+    vector<int> dr = {-1, 1, 0, 0};
+    vector<int> dc = {0, 0, -1, 1};
 public:
-    int solve(vector<vector<int>>& grid,int k){
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
-        queue<vector<int>> q;
-	
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto t=q.front();
-            int x=t[0],y=t[1];
-            q.pop();
-			
-			
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
-                continue;
-            }
-			
-            if(x==grid.size()-1 && y==grid[0].size()-1)
-                return t[2];
-
-            if(grid[x][y]==1){
-                if(t[3]>0)
-                    t[3]--;
-                else
-                    continue;
-            }
-			
-
-            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
-                continue;
-            vis[x][y]=t[3];
-            
-            q.push({x+1,y,t[2]+1,t[3]});
-            q.push({x,y+1,t[2]+1,t[3]});
-            q.push({x-1,y,t[2]+1,t[3]});
-            q.push({x,y-1,t[2]+1,t[3]});
-            
-        }
-        return -1;
-    }
-    
     int shortestPath(vector<vector<int>>& grid, int k) {
-        return solve(grid,k);
+        if (grid[0][0] == 1) return -1;
+        int n = grid.size(), m = grid[0].size();
+        
+        if (k >= m+n-2) return m+n-2;
+        
+        vector<vector<vector<int>>> visited(n, vector<vector<int>>(m, vector<int>(k + 1, 0)));
+        queue<vector<int>> q;
+        q.push({0, 0, 0});
+        visited[0][0][0] = 1;
+        int dist = 0, minDist = INT_MAX;
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                int r = q.front()[0], c = q.front()[1], obs = q.front()[2];
+                q.pop();
+
+                if (r == n - 1 && c == m - 1) {
+                    return dist;
+                }
+
+                for(int i = 0; i < 4; i++) {
+                    int nr = r + dr[i], nc = c + dc[i];
+                    if (nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
+                    if (obs + grid[nr][nc] > k || visited[nr][nc][obs + grid[nr][nc]]) continue;
+
+                    q.push({nr, nc, obs + grid[nr][nc]});
+                    visited[nr][nc][obs + grid[nr][nc]] = 1;
+                }
+            }
+            
+            dist++;
+        }
+        
+        return minDist == INT_MAX ? -1 : minDist;
     }
 };
